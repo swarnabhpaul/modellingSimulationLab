5e88be794132b74ad7715373d94656386c8726a4
swarnabhpaul <swarnabh38paul40@gmail.com>
2022-08-25 16:44:08 +0530

    lab 2

--- Python/Lab files/Lab2Q2.py
+++ Python/Lab files/Lab2Q2.py
@@ -1,4 +1,5 @@
 import numpy as np
+MAX_INT = 2147483647
 
 
 def checkForBalanced(supply, demand):
@@ -16,9 +17,53 @@ def printMatrix(cost, r, c):
 
 
 def lcm(supply, demand, noOfWarehouses, noOfStores, cost, allocation):
-    min_cost = np.amin(cost)
-    while min_cost != np.inf:
-        result = np.where()
+    minCost = np.amin(cost)
+    totalCost = 0
+    noOfAllocations = 0
+    while minCost != MAX_INT:
+        result = np.where(cost == minCost)
+        indexes = list(zip(result[0], result[1]))
+        maxallind = 0
+        maxalloc = 0
+        ind = 0
+        for i, j in indexes:
+            if min(supply[i], demand[j]) > maxalloc:
+                maxalloc = min(supply[i], demand[j])
+                maxallind = ind
+            ind += 1
+        i = indexes[maxallind][0]
+        j = indexes[maxallind][1]
+        allocation[i][j] += min(supply[i], demand[j])
+        supply[i] -= allocation[i][j]
+        demand[j] -= allocation[i][j]
+        totalCost += allocation[i][j]*cost[i][j]
+        if supply[i] < demand[j]:
+            j1 = 0
+            while j1 < noOfStores:
+                cost[i][j1] = MAX_INT
+                j1 += 1
+        elif supply[i] > demand[j]:
+            i1 = 0
+            while i1 < noOfWarehouses:
+                cost[i1][j] = MAX_INT
+                i1 += 1
+        else:
+            j1 = 0
+            while j1 < noOfStores:
+                cost[i][j1] = MAX_INT
+                j1 += 1
+            i1 = 0
+            while i1 < noOfWarehouses:
+                cost[i1][j] = MAX_INT
+                i1 += 1
+        noOfAllocations += 1
+        minCost = np.amin(cost)
+
+    if noOfWarehouses+noOfStores-1 == noOfAllocations:
+        print("\nnon-degeneracy condition is satisfied")
+    else:
+        print("\ndegeneracy condition is satisfied")
+    return totalCost
 
 
 if __name__ == "__main__":
@@ -30,9 +75,10 @@ if __name__ == "__main__":
                      [7, 3, 7, 7, 5, 5],
                      [6, 5, 9, 11, 3, 11],
                      [6, 8, 11, 2, 2, 10]])
-    allocation = np.array([[0, 0, 0, 0],
-                           [0, 0, 0, 0],
-                           [0, 0, 0, 0]])
+    allocation = np.array([[0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0]])
     checkForBalanced(supply, demand)
     print("Supply array:", supply)
     print("Demand array:", demand)
